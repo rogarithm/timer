7f3018367cd15cd65b1c7c1d138d094a7f5e91f9
sehun-kim <rogarithm@gmail.com>
2023-03-31 00:40:35 +0900

    작은 스크립트를 결합해서 타이머를 실행한다
     - 기존 구현한 코드의 함수를 개별 스크립트로 분리한다.
       순서대로 스크립트를 실행하고, 이전 스크립트의 실행 결과를
       다음 스크립트의 입력으로 쓰도록 했다.
     - 테스트하기 편하도록 초 단위 옵션을 추가했다.
     - 옵션 입력 시 -m 10보다 -m10 형식으로 바꿨다.

---
+++ divide/display_badge.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+input=$(cat -)
+exitstatus=$(echo $input | cut -d, -f1)
+elapsed=$(echo $input | cut -d, -f2)
+started_at=$(echo $input | cut -d, -f3) 
+ends_at=$(echo $input | cut -d, -f4)
+
+
+display_badge() {
+
+	local title="완료: 약 $(($elapsed / 60))m $(($elapsed % 60))s"
+	local text="${started_at}-${ends_at}"
+
+	echo display notification \"${text}\" with title \"${title}\" | osascript
+	echo $input
+	exit 0
+}
+
+display_badge
---
+++ divide/start_session.sh
@@ -0,0 +1,10 @@
+#./wait_for.sh -m25 | ./display_badge.sh | ./save_log.sh | ./notify_with_sound.sh
+#./wait_for.sh -m7 | ./display_badge.sh | ./notify_with_sound.sh
+#./wait_for.sh -m25 | ./display_badge.sh | ./save_log.sh | ./notify_with_sound.sh
+#./wait_for.sh -m7 | ./display_badge.sh | ./notify_with_sound.sh
+#./wait_for.sh -m25 | ./display_badge.sh | ./save_log.sh | ./notify_with_sound.sh
+#./wait_for.sh -m7 | ./display_badge.sh | ./notify_with_sound.sh
+
+./wait_for.sh -s3 | ./display_badge.sh "완료" #| ./save_log.sh | ./notify_with_sound.sh
+./wait_for.sh -s3 | ./display_badge.sh #| ./notify_with_sound.sh
+#./wait_for.sh -s10 | ./display_badge.sh | ./save_log.sh | ./notify_with_sound.sh
